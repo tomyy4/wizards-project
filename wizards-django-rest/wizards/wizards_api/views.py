1aecd737262a5e8cd484c1e3e79e9b8cbb6c2c97
tomeu <tomeullompartpiza@gmail.com>
2020-01-15 20:20:48 +0100

    api_views http status

--- wizards-django-rest/wizards/wizards_api/views.py
+++ wizards-django-rest/wizards/wizards_api/views.py
@@ -1,84 +1,81 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Wizard, House
 from .serializers import WizardSerializer, HouseSerializer
 
-@csrf_exempt
+
+@api_view(['GET','POST'])
 def wizards_list(request):
     if request.method == 'GET':
         wizards = Wizard.objects.all()
         serializer = WizardSerializer(wizards, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data, safe=False)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = WizardSerializer(data=data)
+        serializer = WizardSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET','PUT','DELETE'])
 def wizard_detail(request, pk):
     try:
         wizard = Wizard.objects.get(pk=pk)
     except Wizard.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = WizardSerializer(wizard)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = WizardSerializer(wizard, data=data)
+        serializer = WizardSerializer(wizard, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         wizard.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@csrf_exempt
+@api_view(['GET','POST'])
 def house_list(request):
     if request.method == 'GET':
         houses = House.objects.all()
         serializer = HouseSerializer(houses, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data, safe=False)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = HouseSerializer(data=data)
+        serializer = HouseSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET','PUT','DELETE'])
 def house_detail(request, pk):
     try:
         house = House.objects.get(pk=pk)
     except House.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = HouseSerializer(house)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = WizardSerializer(house, data=data)
+        serializer = WizardSerializer(house, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         house.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
