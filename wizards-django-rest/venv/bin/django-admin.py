be70ee3321427a6ea6991ebda7dafc638b4cfbab
tomeu <tomeullompartpiza@gmail.com>
2022-01-13 22:31:12 +0100

    remove env

--- wizards-django-rest/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/tomeu/projects/wizards-project/wizards-django-rest/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
